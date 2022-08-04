5739e68e21764db6f36019a5ddd0e4fd10fc53b3
skai7ami <skai7ami@gmail.com>
2022-08-04 10:57:14 -0300

    gitignore

---
+++ lib/config/api_constants.dart
@@ -0,0 +1,5 @@
+/* Esse arquivo não deve ser enviado para o git
+ ou qualquer utilitário de versionamento de código,
+ por motivos de segurança e privacidade */
+
+const baseUrl = "http://192.168.3.251:8080";
