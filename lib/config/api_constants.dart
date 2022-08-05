ef9161bb7fb4f99b72582444955a5e4f96c70465
João Victor Justino De Souza <109907644+SigmaDevelopment2022@users.noreply.github.com>
2022-08-05 13:17:28 -0300

    Delete api_constants.dart

--- lib/config/api_constants.dart
+++
@@ -1,5 +0,0 @@
-/* Esse arquivo não deve ser enviado para o git
- ou qualquer utilitário de versionamento de código,
- por motivos de segurança e privacidade */
-
-const baseUrl = "http://192.168.3.251:8080";
